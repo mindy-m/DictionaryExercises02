9d5be48ef1c558a7b79e3cd7c81c2891df5e53e8
mindym1 <76831690+mindy-m@users.noreply.github.com>
2021-09-17 02:57:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+import random
+
+# Code your assign_tickets function here:
+
+
+# Code the fix_tickets function here:
+
+
+def main():
+  names = ['Caleb', 'Naomi', 'Owen', 'Ava', 'Aaron', 'Lydia']
+
+  
+if __name__ == '__main__':
+  main()
